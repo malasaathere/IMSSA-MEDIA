f57c9186dba342473edf16cd14f3146568858a2f
malasaathere <rajasoo-im23058@stu.kln.ac.lk>
2026-05-17 13:15:00 +0530

    fix: moved cache clear to index.html

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,7 @@ import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
 import './index.css';
 
-// Change this version number every time you deploy
-const APP_VERSION = "1.0.1";
 
-if (localStorage.getItem("app_version") !== APP_VERSION) {
-  localStorage.clear();
-  sessionStorage.clear();
-  localStorage.setItem("app_version", APP_VERSION);
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
